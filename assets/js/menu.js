4f3c1dd4965fd4fea1c4b6b7920df18945f4ac82
Jared Tomeck <jared.tomeck@kitware.com>
2020-12-31 13:50:55 -0500

    Final navmenu fix

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
   $('.burger').click(function(){
-    $('.navmenu').toggleClass('open');
+    if( $('.navmenu').hasClass('-left-64') ){
+      $('.navmenu').removeClass('-left-64');
+      $('.navmenu').addClass('left-0');
+    } else {
+      $('.navmenu').removeClass('left-0');
+      $('.navmenu').addClass('-left-64');
+    }
   });
 });
