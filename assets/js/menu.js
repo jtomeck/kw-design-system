4ee4169cb5a2570fc91db1097289df36208f7a2d
Jared Tomeck <jared.tomeck@kitware.com>
2020-12-31 14:41:28 -0500

    Add transition properties

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
   $('.burger').click(function(){
     if( $('.navmenu').hasClass('-left-64') ){
       $('.navmenu').removeClass('-left-64');
-      $('.navmenu').addClass('left-0');
+      $('.navmenu').addClass('left-0 shadow-2xl');
     } else {
-      $('.navmenu').removeClass('left-0');
+      $('.navmenu').removeClass('left-0 shadow-2xl');
       $('.navmenu').addClass('-left-64');
     }
   });
