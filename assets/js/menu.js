50fed8ca483a54d1888d2a2c9ea6e4f426dd7857
Jared Tomeck <jared.tomeck@kitware.com>
2022-01-17 14:28:18 -0500

    Animate hamburger

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
   $('.burger-button, .nav-overlay').click(function(){
     if( $('.navmenu').hasClass('closed') ){
+      $('.burger').addClass('open');
       $('.navmenu').stop().animate({left: '0'},200);
     } else {
+      $('.burger').removeClass('open');
       $('.navmenu').stop().animate({left: '-16rem'},200);
     }
     $('.navmenu').toggleClass('closed shadow-2xl');
